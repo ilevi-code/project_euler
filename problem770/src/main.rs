2a91c4c30e378c6e22f8201ce6c90e6c63a2eb60
ilevy <itayill01@gmail.com>
2024-03-23 11:11:27 +0200

    Use MPFR for floating point

--- problem770/src/main.rs
+++ problem770/src/main.rs
@@ -1,9 +1,10 @@
-use std::collections::HashMap;
+use rug::{Assign, Float};
+use std::collections::BTreeMap;
 
-#[derive(Eq, Hash, PartialEq, Debug)]
+#[derive(Eq, PartialOrd, Ord, PartialEq, Debug)]
 struct Actions {
-    gives: usize,
-    takes: usize,
+    gives: i32,
+    takes: i32,
 }
 
 impl Actions {
@@ -21,7 +22,7 @@ impl Actions {
         }
     }
 
-    fn rounds(rounds: usize) -> Actions {
+    fn rounds(rounds: i32) -> Actions {
         Actions {
             gives: rounds,
             takes: rounds,
@@ -29,15 +30,17 @@ impl Actions {
     }
 }
 
-fn delphi(cache: &mut HashMap<Actions, f64>, actions: Actions) -> f64 {
+fn delphi(cache: &mut BTreeMap<Actions, Float>, actions: Actions) -> Float {
     if actions.gives == 0 {
         // offering zeros, not letting the other player get anything
-        return 1.0;
+        return Float::with_val(50, 1.0);
     } else if actions.takes == 0 {
         // offering always the whole amoung, doubling it each time
-        return (actions.gives as f64).exp2();
+        let mut ret = Float::new(50);
+        ret.assign(Float::i_exp(1, actions.gives));
+        return ret;
     } else if let Some(factor) = cache.get(&actions) {
-        return *factor;
+        return factor.clone();
     }
 
     // calculate gain-factor based on possible action of the other player
@@ -48,57 +51,61 @@ fn delphi(cache: &mut HashMap<Actions, f64>, actions: Actions) -> f64 {
     // G+Go = T-To
     // (T+G)o = T-G
     // o = (T-G)/(T+G)
-    let offer = (take_factor - give_factor) / (take_factor + give_factor);
-    let gain = (1.0 + offer) * give_factor;
-    cache.insert(actions, gain);
+    let mut sub = Float::new(50);
+    let mut add = Float::new(50);
+    sub.assign(&take_factor - &give_factor);
+    add.assign(take_factor + &give_factor);
+    let offer = sub / add;
+    let gain: Float = (1.0 + offer) * give_factor;
+    cache.insert(actions, gain.clone());
     gain
 }
 
 #[cfg(test)]
 mod tests {
-    use std::collections::HashMap;
+    use assert_float_eq::{afe_is_f64_near, afe_near_error_msg, assert_f64_near};
+    use std::collections::BTreeMap;
 
     use super::Actions;
 
     fn delphi(actions: Actions) -> f64 {
-        let mut cache = HashMap::new();
-        super::delphi(&mut cache, actions)
+        let mut cache = BTreeMap::new();
+        super::delphi(&mut cache, actions).to_f64()
     }
 
     #[test]
     fn only_gives() {
-        assert_eq!(delphi(Actions { gives: 2, takes: 0 }), 4.0);
+        assert_f64_near!(delphi(Actions { gives: 2, takes: 0 }), 4.0);
     }
 
     #[test]
     fn only_takes() {
-        assert_eq!(delphi(Actions { gives: 0, takes: 3 }), 1.0);
+        assert_f64_near!(delphi(Actions { gives: 0, takes: 3 }), 1.0);
     }
 
     #[test]
     fn single_round() {
-        assert_eq!(delphi(Actions { gives: 1, takes: 1 }), 4.0 / 3.0);
+        assert_f64_near!(delphi(Actions { gives: 1, takes: 1 }), 4.0 / 3.0);
     }
 
     #[test]
     fn single_take_two_gives() {
-        assert_eq!(delphi(Actions { gives: 2, takes: 1 }), 2.0);
+        assert_f64_near!(delphi(Actions { gives: 2, takes: 1 }), 2.0);
     }
 
     #[test]
     fn two_take_single_gives() {
-        assert_eq!(delphi(Actions { gives: 1, takes: 2 }), 8.0 / 7.0);
+        assert_f64_near!(delphi(Actions { gives: 1, takes: 2 }), 8.0 / 7.0);
     }
 
     #[test]
     fn two_rounds() {
-        use assert_float_eq::{afe_is_f64_near, afe_near_error_msg, assert_f64_near};
         assert_f64_near!(delphi(Actions::rounds(2)), 16.0 / 11.0);
     }
 }
 
 fn main() {
-    let mut cache = HashMap::new();
+    let mut cache = BTreeMap::new();
     for rounds in 1.. {
         let gain = delphi(&mut cache, Actions::rounds(rounds));
         println!("{rounds} = {gain}");
