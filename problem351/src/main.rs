1458c3bab705430981ee71b7ef4e5ed35021b838
ilevy <itayill01@gmail.com>
2025-01-06 00:46:50 +0200

    Generating coprimes the easy way
    Still not good enough

--- problem351/src/main.rs
+++ problem351/src/main.rs
@@ -1,90 +1,28 @@
-use itertools::Itertools;
-use rayon::prelude::*;
-use std::hint::black_box;
+use std::collections::VecDeque;
 
-fn list_divisors(mut k: u32) -> Vec<u32> {
-    let mut divisors = Vec::new();
-    divisors.reserve(8);
-    let mut i = 2;
-    while k != 1 {
-        if k % i == 0 {
-            divisors.push(i);
-            while k % i == 0 {
-                k /= i;
+fn check_part(k: u32) -> u64 {
+    let mut v: VecDeque<(u32, u32)> = VecDeque::new();
+    v.push_back((2, 1));
+    v.push_back((3, 1));
+    let mut hidden = 0;
+    while v.len() != 0 {
+        let (m, n) = v.pop_front().unwrap();
+        let f = ((k / (n + m)) * 2 - 2) as u64;
+        hidden += f;
+        let generated = [(2 * m - n, m), (2 * m + n, m), (m + 2 * n, n)];
+        for (m, n) in generated {
+            if m + n <= k / 2 {
+                v.push_back((m, n));
             }
         }
-        i += 1;
     }
-    divisors
-}
-
-fn count_divisible_by_divisors(k: u32, limit: u32) -> u32 {
-    let mut not_divisible = 0;
-    for divisors in list_divisors(k).into_iter().powerset() {
-        let product = divisors.iter().product::<u32>();
-        if divisors.len() == 0 {
-            continue;
-        } else if divisors.len() & 1 == 0 {
-            not_divisible -= (limit / product) as i32;
-            // println!("- {}", limit / product);
-        } else {
-            not_divisible += (limit / product) as i32;
-            // println!("+ {}", limit / product);
-        }
-    }
-    not_divisible as u32
-}
-
-fn check_part(size: u32) -> u64 {
-    let mut hidden = (size - 1) as u64;
-    hidden += (2..size)
-        .into_par_iter()
-        .map(|i| {
-            if i % 10_000 == 0 {
-                println!("{i}");
-            }
-            count_divisible_by_divisors(i, size - i) as u64
-        })
-        .sum::<u64>();
+    hidden += (k - 1) as u64;
+    hidden += ((k - 2) / 2) as u64;
     hidden
 }
 
 fn main() {
-    println!("{}", check_part(100_000));
-}
-
-#[test]
-fn not_divisible_by_1() {
-    assert_eq!(count_divisible_by_divisors(1, 5), 0);
-    assert_eq!(count_divisible_by_divisors(1, 70), 0);
-}
-
-#[test]
-fn not_divisible_by_2() {
-    assert_eq!(count_divisible_by_divisors(2, 1), 0);
-    assert_eq!(count_divisible_by_divisors(2, 2), 1);
-    assert_eq!(count_divisible_by_divisors(2, 3), 1);
-    assert_eq!(count_divisible_by_divisors(2, 4), 2);
-    assert_eq!(count_divisible_by_divisors(2, 70), 35);
-}
-
-#[test]
-fn not_divisible_by_3() {
-    assert_eq!(count_divisible_by_divisors(3, 1), 0);
-    assert_eq!(count_divisible_by_divisors(3, 2), 0);
-    assert_eq!(count_divisible_by_divisors(3, 3), 1);
-    assert_eq!(count_divisible_by_divisors(3, 4), 1);
-    assert_eq!(count_divisible_by_divisors(3, 70), 23);
-}
-
-#[test]
-fn not_divisible_by_6() {
-    assert_eq!(count_divisible_by_divisors(6, 10), 7);
-    assert_eq!(count_divisible_by_divisors(6, 11), 7);
-    assert_eq!(count_divisible_by_divisors(6, 12), 8);
-    assert_eq!(count_divisible_by_divisors(6, 13), 8);
-    assert_eq!(count_divisible_by_divisors(6, 14), 9);
-    assert_eq!(count_divisible_by_divisors(6, 15), 10);
+    println!("{}", check_part(100_000) * 6);
 }
 
 #[test]
